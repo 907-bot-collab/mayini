39071889391a4c5281726a913a2675f15019f63b
907-bot-collab <vc095713@gmail.com>
2025-10-08 20:17:52 +0530

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,11 @@
+# Create an __init__.py file for the tests package
+init_content = '''"""
+Test package for mayini deep learning framework.
+"""
+'''
 
+# Save __init__.py
+with open('test__init__.py', 'w') as f:
+    f.write(init_content)
+
+print("Created test __init__.py file")

19332755b073bbd3a3ab54ce6b17ff6448e007f1
907-bot-collab <vc095713@gmail.com>
2025-10-20 16:53:46 +0530

    Update __init__.py

--- src/mayini/neat/__init__.py
+++ src/mayini/neat/__init__.py
@@ -1,1 +1,8 @@
 
+"""mayini/neat/__init__.py"""
+from .genome import Genome
+from .population import Population
+from .network import NeuralNetwork
+from .config import Config
+
+__all__ = ['Genome', 'Population', 'NeuralNetwork', 'Config']

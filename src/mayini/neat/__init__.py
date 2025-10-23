b117f7cbf0203e3cce64d5ce756ba345cccbcabe
907-bot-collab <vc095713@gmail.com>
2025-10-24 00:29:16 +0530

    Update __init__.py

--- src/mayini/neat/__init__.py
+++ src/mayini/neat/__init__.py
@@ -1,8 +1,32 @@
-
-"""mayini/neat/__init__.py"""
+from .gene import NodeGene, ConnectionGene
 from .genome import Genome
-from .population import Population
+from .innovation import InnovationTracker
 from .network import NeuralNetwork
+from .population import Population
+from .species import Species
+from .activation import ActivationFunctions
 from .config import Config
+from .evaluator import Evaluator, XORFitnessEvaluator
+from .visualization import NEATVisualizer
 
-__all__ = ['Genome', 'Population', 'NeuralNetwork', 'Config']
+__all__ = [
+    # Core classes
+    'NodeGene',
+    'ConnectionGene',
+    'Genome',
+    'InnovationTracker',
+    'NeuralNetwork',
+    'Population',
+    'Species',
+    
+    # Configuration and utilities
+    'Config',
+    'ActivationFunctions',
+    
+    # Evaluation
+    'Evaluator',
+    'XORFitnessEvaluator',
+    
+    # Visualization
+    'NEATVisualizer',
+]

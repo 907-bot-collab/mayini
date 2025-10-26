d7eb85bfa2309c995a4491c778dd9365c930320e
907-bot-collab <vc095713@gmail.com>
2025-10-26 15:19:05 +0530

    Update knn.py

--- src/mayini/ml/supervised/knn.py
+++ src/mayini/ml/supervised/knn.py
@@ -6,8 +6,8 @@ class KNeighborsClassifier(BaseClassifier):
     """
     K-Nearest Neighbors Classifier
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     k : int, default=5
         Number of neighbors to use
     metric : str, default='euclidean'
@@ -15,15 +15,15 @@ class KNeighborsClassifier(BaseClassifier):
     weights : str, default='uniform'
         Weight function ('uniform' or 'distance')
 
-    Example:
-    --------
+    Example
+    -------
     >>> from mayini.ml import KNeighborsClassifier
     >>> knn = KNeighborsClassifier(k=5)
     >>> knn.fit(X_train, y_train)
     >>> predictions = knn.predict(X_test)
     """
 
-    def __init__(self, k=5, metric='euclidean', weights='uniform'):
+    def __init__(self, k=5, metric="euclidean", weights="uniform"):
         super().__init__()
         self.k = k
         self.metric = metric
@@ -42,10 +42,12 @@ class KNeighborsClassifier(BaseClassifier):
 
     def _compute_distances(self, X):
         """Compute distances between X and training data"""
-        if self.metric == 'euclidean':
+        if self.metric == "euclidean":
             # Euclidean distance
-            distances = np.sqrt(np.sum((X[:, np.newaxis] - self.X_train) ** 2, axis=2))
-        elif self.metric == 'manhattan':
+            distances = np.sqrt(
+                np.sum((X[:, np.newaxis] - self.X_train) ** 2, axis=2)
+            )
+        elif self.metric == "manhattan":
             # Manhattan distance
             distances = np.sum(np.abs(X[:, np.newaxis] - self.X_train), axis=2)
         else:
@@ -61,7 +63,7 @@ class KNeighborsClassifier(BaseClassifier):
         distances = self._compute_distances(X)
 
         # Find k nearest neighbors
-        k_indices = np.argsort(distances, axis=1)[:, :self.k]
+        k_indices = np.argsort(distances, axis=1)[:, : self.k]
 
         # Get labels of k nearest neighbors
         k_nearest_labels = self.y_train[k_indices]
@@ -69,11 +71,11 @@ class KNeighborsClassifier(BaseClassifier):
         # Predict based on majority vote
         predictions = []
         for i, neighbors in enumerate(k_nearest_labels):
-            if self.weights == 'uniform':
+            if self.weights == "uniform":
                 # Simple majority vote
                 unique, counts = np.unique(neighbors, return_counts=True)
                 predictions.append(unique[counts.argmax()])
-            elif self.weights == 'distance':
+            elif self.weights == "distance":
                 # Distance-weighted vote
                 neighbor_distances = distances[i, k_indices[i]]
                 # Avoid division by zero
@@ -93,19 +95,19 @@ class KNeighborsClassifier(BaseClassifier):
         X, _ = self._validate_input(X)
 
         distances = self._compute_distances(X)
-        k_indices = np.argsort(distances, axis=1)[:, :self.k]
+        k_indices = np.argsort(distances, axis=1)[:, : self.k]
         k_nearest_labels = self.y_train[k_indices]
 
         probas = []
         for i, neighbors in enumerate(k_nearest_labels):
             class_probas = np.zeros(len(self.classes_))
 
-            if self.weights == 'uniform':
+            if self.weights == "uniform":
                 for label in neighbors:
                     class_idx = np.where(self.classes_ == label)[0][0]
                     class_probas[class_idx] += 1
                 class_probas /= self.k
-            elif self.weights == 'distance':
+            elif self.weights == "distance":
                 neighbor_distances = distances[i, k_indices[i]]
                 weights = 1 / (neighbor_distances + 1e-10)
                 total_weight = np.sum(weights)
@@ -124,8 +126,8 @@ class KNeighborsRegressor(BaseRegressor):
     """
     K-Nearest Neighbors Regressor
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     k : int, default=5
         Number of neighbors to use
     metric : str, default='euclidean'
@@ -133,15 +135,15 @@ class KNeighborsRegressor(BaseRegressor):
     weights : str, default='uniform'
         Weight function ('uniform' or 'distance')
 
-    Example:
-    --------
+    Example
+    -------
     >>> from mayini.ml import KNeighborsRegressor
     >>> knn = KNeighborsRegressor(k=5)
     >>> knn.fit(X_train, y_train)
     >>> predictions = knn.predict(X_test)
     """
 
-    def __init__(self, k=5, metric='euclidean', weights='uniform'):
+    def __init__(self, k=5, metric="euclidean", weights="uniform"):
         super().__init__()
         self.k = k
         self.metric = metric
@@ -159,9 +161,11 @@ class KNeighborsRegressor(BaseRegressor):
 
     def _compute_distances(self, X):
         """Compute distances between X and training data"""
-        if self.metric == 'euclidean':
-            distances = np.sqrt(np.sum((X[:, np.newaxis] - self.X_train) ** 2, axis=2))
-        elif self.metric == 'manhattan':
+        if self.metric == "euclidean":
+            distances = np.sqrt(
+                np.sum((X[:, np.newaxis] - self.X_train) ** 2, axis=2)
+            )
+        elif self.metric == "manhattan":
             distances = np.sum(np.abs(X[:, np.newaxis] - self.X_train), axis=2)
         else:
             raise ValueError(f"Unknown metric: {self.metric}")
@@ -176,7 +180,7 @@ class KNeighborsRegressor(BaseRegressor):
         distances = self._compute_distances(X)
 
         # Find k nearest neighbors
-        k_indices = np.argsort(distances, axis=1)[:, :self.k]
+        k_indices = np.argsort(distances, axis=1)[:, : self.k]
 
         # Get values of k nearest neighbors
         k_nearest_values = self.y_train[k_indices]
@@ -184,10 +188,10 @@ class KNeighborsRegressor(BaseRegressor):
         # Predict based on average
         predictions = []
         for i, neighbors in enumerate(k_nearest_values):
-            if self.weights == 'uniform':
+            if self.weights == "uniform":
                 # Simple average
                 predictions.append(np.mean(neighbors))
-            elif self.weights == 'distance':
+            elif self.weights == "distance":
                 # Distance-weighted average
                 neighbor_distances = distances[i, k_indices[i]]
                 weights = 1 / (neighbor_distances + 1e-10)

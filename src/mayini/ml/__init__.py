c97cc1a79ddf21f7a3b6292b28703188910c4c36
907-bot-collab <vc095713@gmail.com>
2025-10-26 15:00:31 +0530

    Update __init__.py

--- src/mayini/ml/__init__.py
+++ src/mayini/ml/__init__.py
@@ -2,87 +2,58 @@ from .supervised.linear_models import (
     LinearRegression,
     Ridge,
     Lasso,
-    LogisticRegression
+    LogisticRegression,
 )
 
 from .supervised.tree_models import (
     DecisionTreeClassifier,
     DecisionTreeRegressor,
-    RandomForestClassifier
+    RandomForestClassifier,
 )
 
-from .supervised.knn import (
-    KNeighborsClassifier,
-    KNeighborsRegressor
-)
+from .supervised.knn import KNeighborsClassifier, KNeighborsRegressor
 
-from .supervised.svm import (
-    SVC,
-    SVR
-)
+from .supervised.svm import SVC, SVR
 
-from .supervised.naive_bayes import (
-    GaussianNB,
-    MultinomialNB
-)
+from .supervised.naive_bayes import GaussianNB, MultinomialNB
 
-from .unsupervised.clustering import (
-    KMeans,
-    DBSCAN,
-    AgglomerativeClustering
-)
+from .unsupervised.clustering import KMeans, DBSCAN, AgglomerativeClustering
 
-from .unsupervised.decomposition import (
-    PCA,
-    LDA
-)
+from .unsupervised.decomposition import PCA, LDA
 
-from .ensemble.bagging import (
-    BaggingClassifier,
-    BaggingRegressor
-)
+from .ensemble.bagging import BaggingClassifier, BaggingRegressor
 
-from .ensemble.boosting import (
-    AdaBoostClassifier,
-    GradientBoostingClassifier
-)
+from .ensemble.boosting import AdaBoostClassifier, GradientBoostingClassifier
 
 __all__ = [
     # Linear Models
-    'LinearRegression',
-    'Ridge',
-    'Lasso',
-    'LogisticRegression',
-    
+    "LinearRegression",
+    "Ridge",
+    "Lasso",
+    "LogisticRegression",
     # Trees
-    'DecisionTreeClassifier',
-    'DecisionTreeRegressor',
-    'RandomForestClassifier',
-    
+    "DecisionTreeClassifier",
+    "DecisionTreeRegressor",
+    "RandomForestClassifier",
     # KNN
-    'KNeighborsClassifier',
-    'KNeighborsRegressor',
-    
+    "KNeighborsClassifier",
+    "KNeighborsRegressor",
     # SVM
-    'SVC',
-    'SVR',
-    
+    "SVC",
+    "SVR",
     # Naive Bayes
-    'GaussianNB',
-    'MultinomialNB',
-    
+    "GaussianNB",
+    "MultinomialNB",
     # Clustering
-    'KMeans',
-    'DBSCAN',
-    'AgglomerativeClustering',
-    
+    "KMeans",
+    "DBSCAN",
+    "AgglomerativeClustering",
     # Decomposition
-    'PCA',
-    'LDA',
-    
+    "PCA",
+    "LDA",
     # Ensemble
-    'BaggingClassifier',
-    'BaggingRegressor',
-    'AdaBoostClassifier',
-    'GradientBoostingClassifier',
+    "BaggingClassifier",
+    "BaggingRegressor",
+    "AdaBoostClassifier",
+    "GradientBoostingClassifier",
 ]

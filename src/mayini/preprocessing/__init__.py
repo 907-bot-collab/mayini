b3de50bb440f0f44f8b66fcc7bdedd82a5042e6b
907-bot-collab <vc095713@gmail.com>
2025-10-26 16:03:39 +0530

    Update __init__.py

--- src/mayini/preprocessing/__init__.py
+++ src/mayini/preprocessing/__init__.py
@@ -1,73 +1,48 @@
-from .base import BaseTransformer
-
-# Numerical
+from .categorical.encoders import LabelEncoder, OneHotEncoder, OrdinalEncoder
+from .categorical.target_encoding import TargetEncoder, FrequencyEncoder
 from .numerical.scalers import StandardScaler, MinMaxScaler, RobustScaler
 from .numerical.imputers import SimpleImputer, KNNImputer
 from .numerical.normalizers import Normalizer, PowerTransformer
-
-# Categorical
-from .categorical.encoders import LabelEncoder, OneHotEncoder, OrdinalEncoder
-from .categorical.target_encoding import TargetEncoder, FrequencyEncoder
-
-# Feature Engineering
 from .feature_engineering.polynomial import PolynomialFeatures
 from .feature_engineering.interactions import FeatureInteractions
-
-# Text
-from .text.vectorizers import CountVectorizer, TfidfVectorizer
-
-# Selection
-from .selection.variance import VarianceThreshold
-from .selection.correlation import CorrelationThreshold
-
-# Outlier & Type Conversion
-from .outlier_detection import OutlierDetector
-from .type_conversion import DataTypeConverter
-
-# Pipeline
+from .text.vectorizers import TfidfVectorizer, CountVectorizer
+from .outlier_detection import IsolationForest, LocalOutlierFactor
 from .pipeline import Pipeline
+from .selection.variance import VarianceThreshold
+from .selection.correlation import CorrelationSelector
 from .autopreprocessor import AutoPreprocessor
 
 __all__ = [
-    # Base
-    'BaseTransformer',
-    
-    # Numerical
-    'StandardScaler',
-    'MinMaxScaler',
-    'RobustScaler',
-    'SimpleImputer',
-    'KNNImputer',
-    'Normalizer',
-    'PowerTransformer',
-    
-    # Categorical
-    'LabelEncoder',
-    'OneHotEncoder',
-    'OrdinalEncoder',
-    'TargetEncoder',
-    'FrequencyEncoder',
-    
-    # Feature Engineering
-    'PolynomialFeatures',
-    'FeatureInteractions',
-    
-    # Text
-    'CountVectorizer',
-    'TfidfVectorizer',
-    
-    # Selection
-    'VarianceThreshold',
-    'CorrelationThreshold',
-    
-    # Outlier & Type
-    'OutlierDetector',
-    'DataTypeConverter',
-    
+    # Categorical encoding
+    "LabelEncoder",
+    "OneHotEncoder",
+    "OrdinalEncoder",
+    "TargetEncoder",
+    "FrequencyEncoder",
+    # Numerical scaling
+    "StandardScaler",
+    "MinMaxScaler",
+    "RobustScaler",
+    # Imputation
+    "SimpleImputer",
+    "KNNImputer",
+    # Normalization
+    "Normalizer",
+    "PowerTransformer",
+    # Feature engineering
+    "PolynomialFeatures",
+    "FeatureInteractions",
+    # Text processing
+    "TfidfVectorizer",
+    "CountVectorizer",
+    # Outlier detection
+    "IsolationForest",
+    "LocalOutlierFactor",
     # Pipeline
-    'Pipeline',
-    'AutoPreprocessor',
+    "Pipeline",
+    # Feature selection
+    "VarianceThreshold",
+    "CorrelationSelector",
+    # Auto preprocessing
+    "AutoPreprocessor",
 ]
-
-
-

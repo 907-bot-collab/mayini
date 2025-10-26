7583a07a753d239c548632ba5f91dc14263d3da2
907-bot-collab <vc095713@gmail.com>
2025-10-26 15:54:42 +0530

    Refactor Evaluator class for fitness evaluation
    Refactor Evaluator class to streamline fitness evaluation and remove unused parameters. Update evaluate_population to calculate average fitness and track best genome.

--- src/mayini/neat/evaluator.py
+++ src/mayini/neat/evaluator.py
@@ -1,116 +1,71 @@
 import numpy as np
-from .network import NeuralNetwork
 
 
 class Evaluator:
     """
-    Base evaluator class for fitness evaluation
+    Evaluates fitness of NEAT genomes
+
+    Parameters
+    ----------
+    fitness_function : callable
+        Function that takes a genome and returns fitness score
+
+    Example
+    -------
+    >>> from mayini.neat import Evaluator
+    >>> def fitness_fn(genome):
+    ...     # Evaluate genome
+    ...     return score
+    >>> evaluator = Evaluator(fitness_fn)
+    >>> evaluator.evaluate_population(population)
     """
-    
-    def __init__(self, config=None):
-        self.config = config
-    
-    def evaluate_genome(self, genome, fitness_function):
+
+    def __init__(self, fitness_function):
+        self.fitness_function = fitness_function
+
+    def evaluate_genome(self, genome):
         """
         Evaluate a single genome
-        
-        Parameters:
-        -----------
+
+        Parameters
+        ----------
         genome : Genome
             Genome to evaluate
-        fitness_function : callable
-            Function that takes network and returns fitness
-        
-        Returns:
-        --------
-        float : Fitness value
+
+        Returns
+        -------
+        float
+            Fitness score
         """
-        network = NeuralNetwork(genome)
-        fitness = fitness_function(network)
+        fitness = self.fitness_function(genome)
         genome.fitness = fitness
         return fitness
-    
-    def evaluate_population(self, population, fitness_function, parallel=False):
+
+    def evaluate_population(self, population):
         """
-        Evaluate entire population
-        
-        Parameters:
-        -----------
+        Evaluate all genomes in population
+
+        Parameters
+        ----------
         population : Population
             Population to evaluate
-        fitness_function : callable
-            Fitness function
-        parallel : bool
-            Whether to use parallel evaluation
-        
-        Returns:
-        --------
-        list : List of fitness values
+
+        Returns
+        -------
+        float
+            Average fitness of population
         """
-        if parallel:
-            return self._evaluate_parallel(population, fitness_function)
-        else:
-            return self._evaluate_sequential(population, fitness_function)
-    
-    def _evaluate_sequential(self, population, fitness_function):
-        """Sequential evaluation"""
-        fitnesses = []
+        total_fitness = 0
+
         for genome in population.genomes:
-            fitness = self.evaluate_genome(genome, fitness_function)
-            fitnesses.append(fitness)
-        
-        # Track best genome
-        best_genome = max(population.genomes, key=lambda g: g.fitness)
-        if population.best_genome is None or best_genome.fitness > population.best_genome.fitness:
-            population.best_genome = best_genome.copy()
-        
-        return fitnesses
-    
-    def _evaluate_parallel(self, population, fitness_function):
-        """Parallel evaluation (simple multiprocessing)"""
-        from multiprocessing import Pool, cpu_count
-        
-        n_workers = min(cpu_count(), len(population.genomes))
-        
-        with Pool(n_workers) as pool:
-            args = [(genome, fitness_function) for genome in population.genomes]
-            fitnesses = pool.starmap(self._eval_helper, args)
-        
-        for genome, fitness in zip(population.genomes, fitnesses):
-            genome.fitness = fitness
-        
-        # Track best genome
-        best_genome = max(population.genomes, key=lambda g: g.fitness)
-        if population.best_genome is None or best_genome.fitness > population.best_genome.fitness:
-            population.best_genome = best_genome.copy()
-        
-        return fitnesses
-    
-    @staticmethod
-    def _eval_helper(genome, fitness_function):
-        """Helper for parallel evaluation"""
-        network = NeuralNetwork(genome)
-        return fitness_function(network)
+            fitness = self.evaluate_genome(genome)
+            total_fitness += fitness
 
+        avg_fitness = total_fitness / len(population.genomes)
 
-class XORFitnessEvaluator(Evaluator):
-    """
-    Example: XOR problem fitness evaluator
-    """
-    
-    def __init__(self, config=None):
-        super().__init__(config)
-        self.xor_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-        self.xor_outputs = np.array([0, 1, 1, 0])
-    
-    def evaluate_xor(self, network):
-        """Evaluate network on XOR problem"""
-        error = 0.0
-        for xi, xo in zip(self.xor_inputs, self.xor_outputs):
-            output = network.activate(xi)[0]
-            error += (output - xo) ** 2
-        
-        # Fitness is inverse of error
-        fitness = (4.0 - error) ** 2
-        return fitness
+        # Update best genome
+        best = population.get_best_genome()
+        if population.best_genome is None or best.fitness > population.best_genome.fitness:
+            population.best_genome = best.copy()
 
+        return avg_fitness

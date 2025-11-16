967d1f057d26d7870f841023731ca0c2a825984c
907-bot-collab <vc095713@gmail.com>
2025-11-16 13:59:47 +0530

    Update __init__.py

--- src/mayini/ml/ensemble/__init__.py
+++ src/mayini/ml/ensemble/__init__.py
@@ -2,7 +2,7 @@
 
 from .bagging import BaggingClassifier, BaggingRegressor
 from ..supervised.tree_models import RandomForestClassifier, RandomForestRegressor
-from .boosting import AdaBoost, AdaBoostClassifier, GradientBoosting
+from .boosting import AdaBoost, AdaBoostClassifier, GradientBoosting, AdaBoostRegressor
 
 __all__ = [
     "BaggingClassifier",
@@ -10,6 +10,7 @@ __all__ = [
     "RandomForestClassifier",
     "RandomForestRegressor",
     "AdaBoost",
+    "AdaBoostRegressor",
     "AdaBoostClassifier",
     "GradientBoosting",
 ]

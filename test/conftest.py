f4da9ff87f919ddb4d94b69639ca584ce181d1c6
907-bot-collab <vc095713@gmail.com>
2026-01-30 12:59:23 +0530

    Fix file path for saving conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -109,7 +109,7 @@ def numerical_gradient(func, x, h=1e-5):
 '''
 
 # Save conftest.py
-with open('test/conftest.py', 'w') as f:
+with open('conftest.py', 'w') as f:
     f.write(conftest_content)
     
 print("Created conftest.py with common fixtures and utilities")
